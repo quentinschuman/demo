f8fd70b4fbc634270977c783b158e7d8b94ba8e6
quent <quentinschuman@163.com>
2018-05-10 22:10:45 +0800

    [add]datasource

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import javax.sql.DataSource;
+
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
