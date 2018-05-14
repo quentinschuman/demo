fef875a8a566ef1952f88e92c451437183fdce1e
quent <quentinschuman@163.com>
2018-05-14 23:00:26 +0800

    [add]增加权限控制

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * ProjectName: demo
+ * User: quent
+ * Date: 2018/5/14
+ * Time: 22:11
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("login-error")
+    public String loginError(Model model){
+        model.addAttribute("loginError",true);
+        model.addAttribute("errorMsg","登录失败，用户名或密码错误！");
+        return "login";
+    }
+}
